a4e2f70e63c211bb34895cf5875ef9013075e302
Sebastian Lagomarsino Etchandy - ENS <sebastian.lagomarsino@pcunix117.fing.edu.uy>
2019-05-27 17:45:45 -0300

    Venta con = 0

--- include/Venta.hpp
+++ include/Venta.hpp
@@ -6,16 +6,19 @@ using namespace std;
 //Archivos
 #include "DtProducto.hpp";
 #include "Producto.hpp";
+#include "Facturar.hpp";
 
 class Venta {
 private:
     int numero;
     float descuento;
+    Venta(/*args*/);
 public:
-    virtual void agregarProductoAVenta(Producto prod);
-    virtual bool buscarProducto(Producto p);
-    virtual bool estaFacturada();
-    virtual void eliminarProducto(Producto prod, int cantidad);
-    virtual void facturar();
+    virtual void agregarProductoAVenta(Producto prod) = 0;
+    virtual bool buscarProducto(Producto p) = 0;
+    virtual bool estaFacturada() = 0;
+    void eliminarProducto(Producto prod, int cantidad);
+    void facturar();
     virtual set<DtProducto> obtenerProductos();
+    ~Venta();
 };
