35d4b52e34e668ace4c778e347e7fbca2f505518
Eliana Rosselli Orrico - ENS <eliana.rosselli@pcunix116.fing.edu.uy>
2019-05-27 18:09:44 -0300

    eli

---
+++ include/DtCliente.hpp
@@ -0,0 +1,20 @@
+#ifndef _DTCLIENTE_HPP
+#define _DTCLIENTE_HPP
+
+#include "DtDireccion.hpp"
+
+using namespace std;
+
+
+class DtCliente{
+private:
+   string telefono;
+   string nombre;
+   DtDireccion direccion;
+   //set(DtActualizacion) etapaPedidos
+public:
+    DtFactura(/* args */);
+    ~DtFactura();
+};
+
+#endif
