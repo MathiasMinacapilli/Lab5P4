31972905b3c93edae66ce3c81d22ae428ea1ad1f
Eliana Rosselli Orrico - ENS <eliana.rosselli@pcunix118.fing.edu.uy>
2019-05-28 17:02:43 -0300

    agregue pseudoatributos en mesa

--- include/Mesa.hpp
+++ include/Mesa.hpp
@@ -1,19 +1,31 @@
 #ifndef _MESA_HPP
 #define _MESA_HPP
 
+#include <map>
+
 #include "Empleado.hpp"
+#include "VentaLocal.hpp"
 
 using namespace std;
 
 class Mesa{
 private:
-   int numero;
+    int numero;
+    VentaLocal *ventaActual;
+    map<int, VentaLocal *> ventasPasadas;
 public:
-   Mesa(/* args */);
-   ~Mesa();
-   bool noTieneVentas();
-   bool esDelMozo(int numMozo);
-   void terminarVenta();
+    //constructor y destructor
+    Mesa(int num);
+    ~Mesa();
+    //getters y setters 
+    int getNum();
+    void setVentaActual(VentaLocal *v);
+    Venta *getVentaActual();
+    void agregarAVentasPasadas(VentaLocal *v);
+    //operaciones
+    bool noTieneVentas();
+    bool esDelMozo(int numMozo);
+    void terminarVenta();
 };
 
 #endif
