e99b25c157770ae6d2d5c1a2e89d24af45afeca6
Mathias <mathiasminacapilli@gmail.com>
2019-05-30 19:21:07 -0300

    Implementado DtInfoVenta

--- include/DtInfoVenta.hpp
+++ include/DtInfoVenta.hpp
@@ -1,11 +1,26 @@
 #ifndef _DTINFOVENTA_HPP
 #define _DTINFOVENTA_HPP
 
+//Librerias C
+#include <set>
+using namespace std;
+
+//Archivos
+#include "DtProducto.hpp";
+#include "Etapa.hpp";
+
 class DtInfoVenta {
 private:
-    /* data */
+    int numeroVenta;
+    float descuento;
+    set<DtProducto> datosProducto;
+    Etapa* etapaActual;
 public:
-    DtInfoVenta(/* args */);
+    DtInfoVenta(int numeroVenta, float descuento, set<DtProducto> datosProducto, Etapa* etapaActual);
+    int getNumeroVenta();
+    loat getDescuento();
+    et<DtProducto> getDatosProducto();
+    Etapa* getEtapaActual();
     ~DtInfoVenta();
 };
 
