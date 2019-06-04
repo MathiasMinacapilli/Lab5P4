8cd45dc2acd6c9a9160523439a1e1193518d5e13
Santiago Correa Perini - ENS <scorreaperini98@gmail.com>
2019-06-04 17:48:15 -0300

    Santi arranco controlador mesa, queda bastante

--- include/ControladorMesa.hpp
+++ include/ControladorMesa.hpp
@@ -14,9 +14,13 @@ private:
     ControladorMesa();
     static ControladorMesa *instance;
     map<int, Mesa*> mesas;
+    Mesa* mesa_recordada
 public:
+    //Asignar automáticamente mozos a mesas
     map<int, DtMesasMozo> asignarMozosAMesas();
+    //Iniciar Venta en Mesas - cancelarVenta() - 
     void cancelarVenta();
+    //Facturación de una venta - generarFactura() - ControladorVenta
     void finalizarVenta();
     set<int> getMesasMozoSinVentas(int num_mozo);
     set<int> getMesasSeleccionadas();
---
+++ src/ControladorMesa.cpp
@@ -0,0 +1,11 @@
+//Librerias de C
+using namespace std;
+#include <set>
+#include <map>
+
+//Archivos
+#include "../include/ControladorMesa.hpp"
+
+map<int, DtMesasMozo> asignarMozosAMesas() {
+    
+}
