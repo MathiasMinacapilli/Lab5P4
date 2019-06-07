2c5021c84d35cedc6c2848e976c5c30e5ea5b8fc
Santiago Correa Perini - ENS <scorreaperini98@gmail.com>
2019-06-07 15:31:44 -0300

    Se arranca el principal

--- principal.cpp
+++ principal.cpp
@@ -5,3 +5,11 @@
 ▒█▄▄█ ▀░▀▀ ░▀▀▀ █▀▀▀ ▀▀▀▀ 　 █▄▄█ ▀▄▄▀ 
 ======================================
 */
+
+#include "include/ICliente"
+#include "include/IEmpleado"
+#include "include/IFactura"
+#include "include/IMesa"
+#include "include/IProducto"
+#include "include/IVenta"
+#include "include/Fabrica"
