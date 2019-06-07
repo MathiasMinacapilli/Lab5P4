6f318aa52d99ea6b8923232a66b3a063b3652255
Seba Lagomarsino <lagomarsinosebastian@gmail.com>
2019-06-07 17:38:09 -0300

    Cliente.hpp y .cpp

---
+++ include/Cliente.hpp
@@ -0,0 +1,29 @@
+#ifndef _CLIENTE_HPP
+#define _CLIENTE_HPP
+
+//Librerias de C
+#include <set>
+#include <string>
+using namespace std;
+
+//Archivos
+#include "DtActualizacion.hpp"
+#include "DtDireccion.hpp"
+#include "IObserverActualizaciones.hpp"
+
+class Cliente : public IObserverActualizaciones{
+private:
+    string nombre;
+    string telefono;
+    DtDireccion direccion;
+    string mail;
+    string clave;
+    set<DtActualizacion> etapaPedidos;
+public:
+    void suscribirse();
+    void bajaSuscripcion();
+    void consultarPedidos();
+    void notificar(DtActualizacion actualizacion);
+};
+
+#endif
---
+++ src/Cliente.cpp
@@ -0,0 +1,12 @@
+//Librerias de C
+#include <set>
+#include <string>
+using namespace std;
+
+//Archivos
+#include "../include/Cliente.hpp"
+
+void Cliente::suscribirse() {}
+void Cliente::bajaSuscripcion() {}
+void Cliente::consultarPedidos() {}
+void Cliente::notificar(DtActualizacion actualizacion) {}
